387b5d6b3849dde732d37af6bc49da551cd0a475
Alex <taus.anarchy@gmail.com>
2024-05-01 18:09:22 +0300

    fix: the program thinks the user's saying something although they aren't

---
+++ config.py
@@ -0,0 +1,3 @@
+ENERGY_TRESHOLD = 500  # High by default for low-quality devices and loud environments
+PAUSE_TRESHOLD = 2  # Seconds after which the phrase is considered complete
+PHRASE_TRESHOLD = 2  # Minimum amount of seconds for the audio to be considered a phrase
